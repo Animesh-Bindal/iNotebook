2f09fc7c682a94e327b52828b6615f3a31a3b751
Animesh Bindal <98091040+Animesh-Bindal@users.noreply.github.com>
2024-05-25 23:11:54 +0530

    modifyy

--- src/App.js
+++ src/App.js
@@ -16,22 +16,22 @@ import Login from './components/Login';
 function App() {
   return (
     <>
-        <NoteState>
-          <Router>
-            <Navbar />
-            <Alert message="This is amazing React course" />
-            <div className="container">
-              <Routes>
-                <Route exact path="/" element={<Home />}></Route>
-                <Route exact path="/about" element={<About />}></Route>
-                <Route exact path="/login" element={<Login />}></Route>
-                <Route exact path="/signup" element={<Signup />}></Route>
-              </Routes>
-            </div>
-          </Router>
-        </NoteState>
-      </>
-      );
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Alert message="This is amazing React course" />
+          <div className="container">
+            <Routes>
+              <Route exact path="/" element={<Home />}></Route>
+              <Route exact path="/about" element={<About />}></Route>
+              <Route exact path="/login" element={<Login />}></Route>
+              <Route exact path="/signup" element={<Signup />}></Route>
+            </Routes>
+          </div>
+        </Router>
+      </NoteState>
+    </>
+  );
 }
 
-      export default App;
+export default App;
