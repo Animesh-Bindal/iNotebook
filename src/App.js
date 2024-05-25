2101e880c2ebe74ed9fec56deaa706ab438663d7
Animesh Bindal <98091040+Animesh-Bindal@users.noreply.github.com>
2024-05-25 23:02:41 +0530

    React frontend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import { Home } from './components/Home';
+import About from './components/About';
+import NoteState from './context/notes/NoteState';
+import { Alert } from './components/Alert';
+import Signup from './components/Signup';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is iNotebook</h1>
-      <h2>Hi there</h2>
-    </div>
-  );
+    <>
+        <NoteState>
+          <Router>
+            <Navbar />
+            <Alert message="This is amazing React course" />
+            <div className="container">
+              <Routes>
+                <Route exact path="/" element={<Home />}></Route>
+                <Route exact path="/about" element={<About />}></Route>
+                <Route exact path="/login" element={<Login />}></Route>
+                <Route exact path="/signup" element={<Signup />}></Route>
+              </Routes>
+            </div>
+          </Router>
+        </NoteState>
+      </>
+      );
 }
 
-export default App;
+      export default App;
---
+++ src/components/Signup.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Signup() {
+  return (
+    <div>
+      I am Signup Page
+    </div>
+  )
+}
+
+export default Signup
